27c0398d3cf51cdc37bd2c66c01045ed5ebae94d
korya <korya@github.com>
2014-11-05 11:39:47 -0500

    Add Vagrantfile and bash b2d function

---
+++ b2d.sh
@@ -0,0 +1,50 @@
+_b2d_id() {
+  # XXX The path of boot2docker image is ~/dev/b2d
+  vagrant global-status | awk '/dev\/b2d *$/ { print $1; }'
+}
+
+b2d() {
+  local id=${BOOT2DOCKER_ID}
+  local cmd="$1" && shift
+
+  if test -z "$id" || ! docker version >/dev/null 2>&1; then
+    id=$(_b2d_id)
+  fi
+
+  case "$cmd" in
+
+  ""|config)
+    local ip=$(vagrant ssh-config $id | sed -n "s/[ ]*HostName[ ]*//gp")
+    export DOCKER_HOST="tcp://${ip}:2375"
+    ;;
+
+  ntp)
+    vagrant ssh $id -- sudo /usr/local/bin/ntpclient -s -h pool.ntp.org
+    vagrant ssh $id -- date -u
+    date -u
+    ;;
+
+  id) echo $id ;;
+
+  dir)
+    # XXX The path of boot2docker image is ~/dev/b2d
+    echo "${HOME}/dev/b2d"
+    ;;
+
+  ssh)
+    vagrant ssh $id -- "$@"
+    ;;
+
+  status)
+    vagrant status $id
+    docker version
+    ;;
+
+  edit)
+    gvim "$@" $(b2d dir)/Vagrantfile
+    ;;
+
+  *) vagrant "$cmd" $id "$@" ;;
+
+  esac
+}
